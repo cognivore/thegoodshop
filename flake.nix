8f2dd3c0b7f895bd7dd9a49bdcaac0075b164b78
cognivore <66186054+cognivore@users.noreply.github.com>
2025-03-18 15:40:44 +0000

    Problem: No project for the good shop
    Solution:
    
     - Use flake.nix for most dependencies one can possibly need for this project

---
+++ flake.nix
@@ -0,0 +1,59 @@
+{
+  description = "Flake for building LLM-SEO project with standard nixpkgs Rust and OpenSSL support";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rustc
+            cargo
+            rustfmt
+            clippy
+            rust-analyzer
+            pkg-config
+            sqlx-cli
+            postgresql
+            sqlite
+            nodejs
+            pnpm
+            zip
+            unzip
+            rsync
+            openssl.dev
+            openssl
+
+            chromedriver
+            chromium
+            xvfb-run
+
+            typescript
+            nodePackages.typescript
+
+            rclone
+
+            csvlens
+
+            act
+            docker
+            docker-compose
+
+            shellcheck
+          ];
+        };
+
+        devShell = self.devShells.${system}.default;
+      }
+    );
+}
